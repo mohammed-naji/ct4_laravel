5a9de5dda9c24778e2a359153e32e7fe8fa25d9b
Mohammed Naji <m.naji1512@gmail.com>
2025-11-30 12:12:10 +0200

    start route

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// namespace , use
 
-Route::get('/about', function () {
-    return view('welcome');
+Route::get('/', function () {
+    return 'Homepage';
 });
