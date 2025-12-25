3954af00f05b36e385ba7229b2053b5dae8ad0f7
Mohammed Naji <m.naji1512@gmail.com>
2025-12-25 12:56:42 +0200

    database migration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,5 +22,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         View::share('dev_name', 'Zina Mohammed Naji Abu Alqumbuz');
+
+        Schema::defaultStringLength(191);
     }
 }
